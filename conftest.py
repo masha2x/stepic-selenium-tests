01b78c34e8db2fac048b28f08bcf0e83ac1ab2ce
M <mariia.khurdenko@gmail.com>
2020-04-15 11:22:40 -0500

    added 2 new tests

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 @pytest.fixture(scope="function")
 def browser():
